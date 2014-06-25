2e9b5241cc6fc3621972ef9e30fef29f5f53486e
Khorevich  Dmitry <dmitry.khorevich@altoros.com>
2014-06-25 17:03:04 +0300

    change to work state

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,29 +1,34 @@
-var fileApp = angular.module('fileApp', ['ui.bootstrap','notify']);
+var fileApp = angular.module('fileApp', ['ui.bootstrap', 'notify', 'pascalprecht.translate']);
 var size = [ 'B', 'KB', 'MB' ];
 var names = [ 'Sophia Parkenson', 'Trinity Clapton', 'Katherine Adamson'];
 
-fileApp.controller('FileListCtrl', function ($scope, $http, $filter) {
-    $http.get('data/files.json').success(function(data) {
+fileApp.controller('FileListCtrl', function ($scope, $http, $filter, $translate) {
+    $http.get('data/files.json').success(function (data) {
         $scope.files = data;
     });
+
+    $scope.setLang = function (langKey) {
+        // You can change the language during runtime
+        $translate.use(langKey);
+    };
 
     $scope.addItem = function () {
 
         $scope.files.push({
-            'name':     $scope.fileName,
-            'size':     Math.floor(Math.random()*1000) + ' ' + size[Math.floor(Math.random()*size.length)],
-            'uplby':    names[Math.floor(Math.random()*names.length)],
-            'uplon':     $filter('date')(new Date(),'dd/MM/yyyy')
+            'name': $scope.fileName,
+            'size': Math.floor(Math.random() * 1000) + ' ' + size[Math.floor(Math.random() * size.length)],
+            'uplby': names[Math.floor(Math.random() * names.length)],
+            'uplon': $filter('date')(new Date(), 'dd/MM/yyyy')
         });
 
         $scope.fileName = "";
 
     }
-    $scope.rename=false;
+    $scope.rename = false;
 
     $scope.renameItem = function () {
-        if($scope.selectedFile){
-            $scope.rename=true;
+        if ($scope.selectedFile) {
+            $scope.rename = true;
         }
     }
 
@@ -32,26 +37,26 @@ fileApp.controller('FileListCtrl', function ($scope, $http, $filter) {
         $scope.selectedFile = null;
     }
 
-    $scope.doEnter = function() {
+    $scope.doEnter = function () {
         $scope.selectedFile = null;
-        $scope.rename=false;
+        $scope.rename = false;
     }
 
-    $scope.doEsc = function() {
+    $scope.doEsc = function () {
         $scope.files[$scope.files.indexOf($scope.selectedFile)] = $scope.originalFile;
         $scope.selectedFile = null;
-        $scope.rename=false;
+        $scope.rename = false;
     }
 
     $scope.editFileName = function (file) {
-      //  $scope.selectedFile.editing = false;
+        //  $scope.selectedFile.editing = false;
 //        if($scope.rename==true && file!=$scope.selectedFile){
 //            console.log(file);
 //            console.log($scope.selectedFile);
 //            $scope.doEnter();
 //        }
         $scope.selectedFile = file;
-        $scope.originalFile = angular.extend({},file);
+        $scope.originalFile = angular.extend({}, file);
         console.log("edit");
 //        file.name = 'aa';
         // Clone the original todo to restore it on demand.
@@ -62,15 +67,15 @@ fileApp.controller('FileListCtrl', function ($scope, $http, $filter) {
 fileApp.directive('ngEnter', function () {
     return function (scope, element, attrs) {
         element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
+            if (event.which === 13) {
+                scope.$apply(function () {
                     scope.$eval(attrs.ngEnter);
                 });
 
                 event.preventDefault();
             }
-            if(event.which === 27) {
-                scope.$apply(function (){
+            if (event.which === 27) {
+                scope.$apply(function () {
                     scope.$eval(attrs.ngEsc);
                 });
 
@@ -111,4 +116,19 @@ fileApp.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
         });
     };
 });
+
+fileApp.config(['$translateProvider', function ($translateProvider) {
+
+    // Register a loader for the static files
+    // So, the module will search missing translation tables under the specified urls.
+    // Those urls are [prefix][langKey][suffix].
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'l10n/',
+        suffix: '.json'
+    });
+
+    // Tell the module what language to use by default
+    $translateProvider.preferredLanguage('en_US');
+
+}]);
 
